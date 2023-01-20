d588ee17853127f0850c5b346cd316682c71e703
Museong Hwang <77891754+museonghwang@users.noreply.github.com>
2023-01-20 19:32:21 +0900

    [Bronze I] Title: 평균, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.py
+++ 백준/Bronze/1546. 평균/평균.py
@@ -1,9 +1,5 @@
-n = int(input())
-test_score = list(map(int, input().split()))
-max_score = max(test_score)
+import sys
 
-for i in range(n):
-    test_score[i] = test_score[i] / max_score * 100
-
-result = sum(test_score) / n
-print(result)
+N = int(sys.stdin.readline())
+a = list(map(int, sys.stdin.readline().split()))
+print(sum(a)/max(a)*100/N)
