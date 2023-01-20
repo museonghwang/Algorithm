0940490e40c203cceaf4e26640ba46caebc83e67
Museong Hwang <77891754+museonghwang@users.noreply.github.com>
2023-01-20 19:45:58 +0900

    [Bronze II] Title: OX퀴즈, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/8958. OX퀴즈/OX퀴즈.py
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.py
@@ -1,10 +1,9 @@
 import sys
 
-for i in range(int(sys.stdin.readline())):
+for _ in range(int(sys.stdin.readline())):
     sum = 0; count = 0
-    a = sys.stdin.readline()
-    for j in a:
-        if j == 'O':
+    for i in sys.stdin.readline():
+        if i == 'O':
             count += 1; sum += count
         else: count = 0
     print(sum)
