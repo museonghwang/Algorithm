0c9106d255f1da5686326792c30212ba2c908277
Museong Hwang <77891754+museonghwang@users.noreply.github.com>
2023-01-24 17:34:43 +0900

    [Bronze II] Title: 다이얼, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.py
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -4,8 +4,8 @@ DIAL = ['ABC','DEF','GHI','JKL','MNO','PQRS','TUV','WXYZ']
 time = 0
 
 for i in sys.stdin.readline().rstrip():
-    for j in range(len(DIAL)):
-        if i in DIAL[j]:
-            time += (j+3)
+    for j in DIAL:
+        if i in j:
+            time += DIAL.index(j)+3
 
 print(time)
