df5af02e54b39d30d1f8f8a02d12e486a3f3ec67
Museong Hwang <77891754+museonghwang@users.noreply.github.com>
2023-01-24 18:16:43 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 40 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
@@ -1,14 +1,9 @@
 import sys
 
-ALPHA = ['c=','c-','dz=','d-','lj','nj','s=','z=']
 a = sys.stdin.readline().rstrip()
-
-for i in ALPHA:
-    check = True
-    while check:
-        if i in a:
-            a = a.replace(i, ' ', 1)
-        else:
-            check = False
+for i in ['c=','c-','dz=','d-','lj','nj','s=','z=']:
+    if i in a:
+        a = a.replace(i, ' ')
+    else: continue
 
 print(len(a))
