a83126b63e4d28539f41c36b5a870798e79062a3
Museong Hwang <77891754+museonghwang@users.noreply.github.com>
2023-01-24 18:42:19 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -1,7 +1,7 @@
 import sys
 
 cnt = 0
-for i in range(int(input())):
+for i in range(int(sys.stdin.readline())):
     word = sys.stdin.readline().rstrip()
     if list(word) == sorted(word, key=word.find):
         cnt += 1
