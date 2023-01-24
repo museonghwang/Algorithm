c300d5a57fa5d565ef0ce94b47ce7785af1dba9a
Museong Hwang <77891754+museonghwang@users.noreply.github.com>
2023-01-24 16:00:09 +0900

    [Bronze I] Title: 단어 공부, Time: 76 ms, Memory: 32416 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -1,4 +1,4 @@
-dummy = input()
+dummy = input().upper()
 
-a = [dummy.upper().count(chr(i)) for i in range(65, 91)]
-print('?' if a.count(max(a)) > 1 else chr(a.index(max(a)) + 65))
+a = [dummy.count(chr(i)) for i in range(65, 91)]
+print('?' if a.count(max(a)) > 1 else chr(a.index(max(a))+65))
