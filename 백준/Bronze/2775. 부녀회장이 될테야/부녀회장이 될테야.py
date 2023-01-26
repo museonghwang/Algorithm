cba14158e0144c3333c3fabf4e27d93aa94333d4
Museong Hwang <77891754+museonghwang@users.noreply.github.com>
2023-01-27 02:33:02 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 48 ms, Memory: 32540 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -1,9 +1,10 @@
-import math
+# k층 n호 = (k+n)! / ((n-1)! * (k+1)!)
+# 1층 3호 = 6  = 4C2 = 4! / 2!2!
+# 2층 3호 = 10 = 5C3 = 5! / 2!3!
 
-T = int(input())
+from math import factorial
 
-for i in range(T):
+for i in range(int(input())):
     k = int(input())
     n = int(input())
-    
-    print(int(math.factorial(k+n)/(math.factorial(n-1)*math.factorial(k+1))))
+    print(int(factorial(k+n)/(factorial(n-1)*factorial(k+1))))
