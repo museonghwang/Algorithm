e10a489830565f8c752e53d702262892f3d97496
Museong Hwang <77891754+museonghwang@users.noreply.github.com>
2023-01-27 02:36:50 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 36 ms, Memory: 32540 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -1,8 +1,7 @@
-from math import factorial
+from math import factorial as f
 import sys
 
 input = sys.stdin.readline
 for i in range(int(input())):
-    k = int(input())
-    n = int(input())
-    print(int(factorial(k+n)/(factorial(n-1)*factorial(k+1))))
+    k = int(input()); n = int(input())
+    print(int(f(k+n)/(f(n-1)*f(k+1))))
