25a46f8a7863e5f7aef2640ebdfb588b071403ac
Museong Hwang <77891754+museonghwang@users.noreply.github.com>
2023-01-26 10:56:03 +0900

    [Bronze I] Title: 분수찾기, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/1193. 분수찾기/분수찾기.py
@@ -0,0 +1,18 @@
+X = int(input())
+
+line = 0 
+end = 0
+
+while X > end:
+    line += 1
+    end += line
+
+diff = end - X
+if line % 2 == 0:
+    top = line - diff
+    bottom = diff + 1
+else:
+    top = diff + 1
+    bottom = line - diff
+
+print(f'{top}/{bottom}')
