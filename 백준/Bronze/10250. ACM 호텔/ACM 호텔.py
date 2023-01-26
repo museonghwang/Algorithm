556ef71c327f51f6ec060ceff765b46a87ef7296
Museong Hwang <77891754+museonghwang@users.noreply.github.com>
2023-01-26 22:53:05 +0900

    [Bronze III] Title: ACM 호텔, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -1,10 +1,6 @@
-import sys, math
-for i in range(int(input())):
-    H, W, N = map(int, sys.stdin.readline().split())
-    line = math.ceil(N/H)
-    # 층수
-    while N > H:
-        
-        N -= H
-        
-    print(N*100 + line) # 호수
+import sys
+for _ in range(int(input())):
+    H, _, N = map(int, sys.stdin.readline().split())
+    floor = N%H; line = N//H+1
+    if floor == 0: floor = H; line -= 1
+    print(floor*100 + line)
