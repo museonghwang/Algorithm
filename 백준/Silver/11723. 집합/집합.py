bc7dc9cde4ce7edfd441ae04d0ee6ab81e36d0b3
Museong Hwang <77891754+museonghwang@users.noreply.github.com>
2023-01-31 15:05:57 +0900

    [Silver V] Title: 집합, Time: 2532 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -1,22 +1,23 @@
-from sys import stdin, stdout
-m = int(stdin.readline())
-_set = 0
-for _ in range(m):
-    order = stdin.readline().split()
-    if order[0] == "all":
-        _set = (1 << 20) - 1
-    elif order[0] == "empty":
-        _set = 0
-    else:
-        num = int(order[1]) - 1
-        if order[0] == "add":
-            _set |= (1 << num)
-        elif order[0] == "remove":
-            _set &= ~(1 << num)
-        elif order[0] == "check":
-            if _set & (1 << num):
-                stdout.write("1\n")
-            else:
-                stdout.write("0\n")
+import sys
+m = int(sys.stdin.readline())
+S = 0b0
+all_S = 0b111111111111111111111
+not_S = 0b000000000000000000000
+
+for i in range(m):
+    cmd = sys.stdin.readline().rstrip().split(" ")
+    if cmd[0] == 'add':
+        S |= 1 << int(cmd[-1])
+    elif cmd[0] == 'remove':
+        S &= ~(1 << int(cmd[-1]))
+    elif cmd[0] == 'check':
+        if S & (1 << int(cmd[-1])):
+            sys.stdout.write('1\n')
         else:
-            _set ^= (1 << num)
+            sys.stdout.write("0\n")
+    elif cmd[0] == 'toggle':
+        S ^= 1 << int(cmd[-1])
+    elif cmd[0] == 'all':
+        S = S | all_S
+    elif cmd[0] == 'empty':
+        S = S & not_S
