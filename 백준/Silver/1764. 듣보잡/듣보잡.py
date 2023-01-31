d4d3f7d2dc2d1f603bc07c5710ab08964f317695
Museong Hwang <77891754+museonghwang@users.noreply.github.com>
2023-01-31 23:10:20 +0900

    [Silver IV] Title: 듣보잡, Time: 80 ms, Memory: 37400 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.py
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -1,9 +1,13 @@
-import sys
+import sys 
 input = sys.stdin.readline
 n, m = map(int, input().split())
 
-a = {input().rstrip() for _ in range(n)}
-b = {input().rstrip() for _ in range(m)}
+not_listen = {input() for _ in range(n)}
+ans = []
+for i in range(m):
+    not_seen = input()
+    if not_seen in not_listen:
+        ans.append(not_seen)
 
-print(len(a & b))
-print(*sorted(a & b), sep='\n')
+print(len(ans))
+print(''.join(sorted(ans)))
