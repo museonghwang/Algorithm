8282a2deeed3b328def0a27cc4db31f4bbb5d684
Museong Hwang <77891754+museonghwang@users.noreply.github.com>
2023-02-01 02:35:33 +0900

    [Silver IV] Title: ATM, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -1,11 +1,9 @@
 import sys
 n = int(sys.stdin.readline())
 p = sorted(list(map(int, sys.stdin.readline().split())))
-
-prefix_sum = []
-m = 0
-for i in p:
-    m += i
-    prefix_sum.append(m)
-
-print(sum(prefix_sum))
+Pre=0
+Sum=0
+for i in range(n):
+    Pre += p[i]
+    Sum += Pre
+print(Sum)
