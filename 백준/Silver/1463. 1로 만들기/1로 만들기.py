2cddab9440b0f06a76a3a90662b95be7ab2afff2
Museong Hwang <77891754+museonghwang@users.noreply.github.com>
2023-02-05 23:49:29 +0900

    [Silver III] Title: 1로 만들기, Time: 540 ms, Memory: 39068 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+N = int(input())
+DP = [0]*(N+1)
+DP[1] = 0
+
+for i in range(2, N+1):
+    DP[i] = DP[i-1] + 1
+    if i % 2 == 0: DP[i] = min(DP[i], DP[i//2]+1)
+    if i % 3 == 0: DP[i] = min(DP[i], DP[i//3]+1)
+
+print(DP[N])
